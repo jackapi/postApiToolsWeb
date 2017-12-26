9aa3d12fd4be6189bd99edad80e1e1ee01c47fed
chenran <925054824@qq.com>
2017-12-26 11:26:34 +0800

    初步修改

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 postApiToolsWeb
-- 
+- postApiToolsWeb接口文档系统 - postAPITools软件
+- 发布接口和管理分配接口！
--- application/manage/controller/Index.php
+++ application/manage/controller/Index.php
@@ -1,7 +1,10 @@
 <?php
 
-namespace app\index\controller;
-
+namespace app\manage\controller;
+/**
+ * 后台管理
+ * @author chenran(apizl) <apiziliao@gmail.com>
+ */
 class Index extends \think\Controller {
 
     public function index() {
