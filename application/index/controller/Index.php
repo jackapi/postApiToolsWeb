832d62a601ad5d87ebbeffd9c597b6c2c596b62c
chenran <925054824@qq.com>
2017-12-26 11:37:34 +0800

    测试sqlite数据库

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -7,5 +7,13 @@ class Index extends \think\Controller {
     public function index() {
         return $this->fetch('index');
     }
+    /**
+     * 测试sqlite
+     */
+    public function testSqlite(){
+       $result= \think\Db::table('history')
+                ->find();
+        var_dump($result);
+    }
 
 }
